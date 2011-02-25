ad07cf0d8cc298a9a44037c4b2b5f673c319abe1
Kevin Brown <kevin.brown01@gmail.com>
2011-02-25 14:49:21 -0800

    -changed matrices to arrays in db data -verified that sizes of P_i,P_j,P_ij are OK -started on fitness calculation

--- src/CGASimulation.py
+++ src/CGASimulation.py
@@ -13,18 +13,31 @@ import CGAGenerator
 class CGASimulation(object):
     """Main class that does the simulation, records results, evaluates trees, etc."""
     def __init__(self, databaseFile, pdbFile, forestSize=100, timeSteps=100):
+        if not os.path.exists(pdbFile):
+            raise IOError, 'something is wrong with your pdb file; check yourself . . .'
         database = Utilities.readDatabase(databaseFile)
         self.indices = database['indices']
         self.singleFrequencies = database['singleFrequencies']
         self.jointFrequencies = database['jointFrequencies']
-        if not os.path.exists(pdbFile):
-            raise IOError, 'something is wrong with your pdb file; check yourself . . .'
+        self.prepare_data()
+        # distances and protein diameter do not change
         self.distances = Utilities.calculateAtomicDistances(pdbFile)
+        self.proteinDiameter = mean(self.distances.values()) - min(self.distances.values())
+        self.proteinMinimum = min(self.distances.values())
         self.cgag = CGAGenerator.CGAGenerator()
         # parameters specific to the simulation
         self.population = []
         self.time = 0
         self.forestSize = forestSize
+        
+    def prepare_data(self):
+        # change input frequencies from the db to 20X20 arrays (including single frequencies)
+        for i in range(0,len(self.indices)):
+            indxi = self.indices[i]
+            self.singleFrequencies[indxi] = MATH.tile(MATH.asarray(self.singleFrequencies[indxi]),(1,20))
+            for j in range(i+1,len(self.indices)):
+                indxj = self.indices[j]
+                self.jointFrequencies[(indxi,indxj)] = MATH.asarray(self.jointFrequencies[(indxi,indxj)])
         
     def populate(self,treetype='exponential',p=0.65,treeSize=10):
         """Populate the forest of function trees.  You can choose to either use probabilistic tree
@@ -49,15 +62,30 @@ class CGASimulation(object):
         """Step forward one step in time recording"""
         pass
     
-    def evaluate(self):
-        """Evaluates the fitness of each species in the population."""
-        pass
+    def evaluate_fitness(self,tree):
+        """Accepts an input tree (member of the forest) and evaluates its fitness, currently 
+        defined as:
+            fitness = 1 + accuracy
+        Accuracy is computed in a different function, in order to allow easy swapping in of
+        different definitions."""
+        # figure out which terminal nodes need a data replace (it's the same nodes for all positions,
+        #    and we have no idea how many times (if any) these appear
+        toreplace = {'p_i':list(),'p_j':list(),'p_ij':list()}
+        for t in tree.termini:
+            if toreplace.has_key(t.string):
+                toreplace[t.string].append(t)
+        # evaluate the tree to obtain the dictionary of weights, for all indices
+        for xi in self.indices:
+            for xy in self.indices:
+                # fix the protein data in the terminal nodes which hold protein data
+                pass
+                
 
-    def calculateAccuracy(self):
-        """Calculates the accuracy from an input"""
-        pass
-#        accuracy = []
-#        weights = 0.0
+    def calculate_accuracy(self,weights):
+        """Calculates the accuracy from an input dictionary of weights, keyed on the same indices as the
+        distances matrix."""
+        accuracy = []
+        weights = 0.0
 #        proteinDiameter = mean(self.distances.values()) - min(self.distances.values())
 #        proteinMinimum = min(self.distances.values())
 #        for i,j in something:
@@ -72,20 +100,21 @@ class CGASimulationTests(unittest.TestCase):
         self.mySimulation.populate(treetype='fixed',treeSize=5)
 
     def testPopulation(self):
-        print "\n\n----- testing population generation -----"
+        print "\n----- testing population generation -----"
         for x in self.mySimulation.population:
             x()
-            print 'String rep : %s' %(x.string)
+            print 'String rep : %s, Value : %f' %(x.string,x.function)
 
-    def testSimulation(self):
-        print "\n\n----- testing simulation -----"
+    def testShape(self):
+        print "\n----- testing shape conversion of input data -----"
         print self.mySimulation.indices
-        print 'Type of P_i  : ', type(MATH.asarray(self.mySimulation.singleFrequencies[20]))
-        print 'Type of P_ij : ', type(MATH.asarray(self.mySimulation.jointFrequencies[(7,20)]))
-        print (MATH.asarray(self.mySimulation.singleFrequencies[20]))*log(MATH.asarray(self.mySimulation.singleFrequencies[20]))
-        #print self.mySimulation.jointFrequencies[(7,20)]*log(self.mySimulation.jointFrequencies[(7,20)])
-        #print log(self.mySimulation.jointFrequencies[(7,20)])
-        print self.mySimulation.distances[(7, 20)]
+        print 'P_7(:,1)  : ', self.mySimulation.singleFrequencies[7][:,0]
+        print 'sum(P_7(:,1)) : ', sum(self.mySimulation.singleFrequencies[7][:,0])
+        print 'Dimensions of P_i :          %d X %d' % (self.mySimulation.singleFrequencies[7].shape)
+        print 'Dimensions of P_ij :         %d X %d' % (self.mySimulation.jointFrequencies[(7,20)].shape)
+        print 'Shape of entropy(P_7) :      %d X %d' % ((self.mySimulation.singleFrequencies[7]*log(self.mySimulation.singleFrequencies[7])).shape)
+        print 'Shape of entropy(P_{7,20}) : %d X %d' % ((self.mySimulation.jointFrequencies[(7,20)]*log(self.mySimulation.jointFrequencies[(7,20)])).shape)
+        print 'Distance(7,20) : ' , self.mySimulation.distances[(7, 20)]
     
         
 if __name__ == '__main__':

252468c0df7cf2e3ba5f0fbfbeeea5ee0384df27
Kevin Brown <kevin.brown01@gmail.com>
2011-05-16 11:08:45 -0700

    Minor change.

--- src/cgarun.py
+++ src/cgarun.py
@@ -22,7 +22,7 @@ def run_cga():
     #        fixed = fixed number of nodes
     #        p = tree size (#nonterminal nodes)
     #        r = bias parameter (prob. of Binary vs Unary node)
-    mySim = CGASimulation.CGASimulation(databaseFile=proteinDBFileName, pdbFile=pdbFileName, fitnessMethod='distance_matrix', forestSize=50, treeGenDict={'treetype':'fixed','p':15,'r':0.5})
+    mySim = CGASimulation.CGASimulation(databaseFile=proteinDBFileName, pdbFile=pdbFileName, fitnessMethod='weighted_accuracy', forestSize=50, treeGenDict={'treetype':'fixed','p':15,'r':0.5})
      # create and attach a DataLogger
     dataLogger = CGALogging.SqliteLogger('../tests')
     mySim.attach(dataLogger)
